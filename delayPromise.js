9822822522cc4669cb5f3e357ff28317417867a0
Anna <89338566+Anna-rhosyn@users.noreply.github.com>
2023-02-01 12:31:15 +0530

    Delete delayPromise.js

--- delayPromise.js
+++
@@ -1,9 +0,0 @@
-function delay(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-delay(3000)
-  .then(() => console.log("this is delay function"))
-  .catch((error) => {
-    console.log(error);
-  });
